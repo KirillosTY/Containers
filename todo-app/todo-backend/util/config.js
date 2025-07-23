10159072f4d6b3506ec77e0e761d56be2b7b967f
KirillosTY <kirillos.important@gmail.com>
2025-07-23 16:18:44 +0300

    part 12.20 done

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const MONGO_URL = process.env.MONGO_URL || undefined
 const REDIS_URL = process.env.REDIS_URL || undefined
 
-console.log('MONGO_URL', REDIS_URL)
+console.log('MONGO_URL', MONGO_URL)
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
   REDIS_URL//: '//localhost:6378'
